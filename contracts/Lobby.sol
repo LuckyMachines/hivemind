25d52f45874f6355b80b39b18513c792a7f6938b
Max Meier <max@studiomax.io>
2022-08-04 16:04:48 -0700

    lobby stub

--- contracts/Lobby.sol
+++ contracts/Lobby.sol
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "hardhat/console.sol";
+
+contract Lobby is AccessControlEnumerable {
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+}
+
 // View available games
 // Choose game to play / join
 // move with group into round 1 when game is ready
